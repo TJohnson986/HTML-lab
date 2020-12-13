f87be9c2ec844e56d66fbde59360c19f9d59553e
Tyler Johnson <tyler@csdconsultants.us>
2020-12-13 14:59:05 -0800

    added variables and if else statements

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("hello, world");
+
+var promptQuestion = "Do you like rain?";
+var response = prompt(promptQuestion);
+console.log(response);
+
+var alertMessage;
+
+if (response === "yes") {
+    alertMessage = "The PNW is for you!";
+} else if (response === "no") {
+    alertMessage = "Here's an umbrella ☂️"
+}
