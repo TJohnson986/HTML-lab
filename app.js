5c6c0e20e4db8569fd46083b837d96cbfc41029b
Tyler Johnson <tyler@csdconsultants.us>
2020-12-14 22:16:45 -0800

    added response = response.toLowerCase to responseInput function. Removed additional if else statements that specified capital letters.

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ console.log(response);
 
 var alertMessage;
 
+
+
+
 function responseInput() {
+    response = response.toLowerCase();
     if (response === "yes") {
         alertMessage = "The PNW is for you!";
-    } else if (response === "Yes") {
-        alertMessage = "The PNW is for you!";
     } else if (response === "no") {
-        alertMessage = "Here's an umbrella ☂️"
-    } else if (response === "No") {
         alertMessage = "Here's an umbrella ☂️"
     }
     alert (alertMessage)
 }
 
-responseInput ();
+responseInput();
 
